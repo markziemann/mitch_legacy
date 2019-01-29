c505c9f3df0a6a53b0b8f3838cb4ffa27cc4e60b
markziemann <mark.ziemann@gmail.com>
2019-01-29 14:16:52 +1100

    separate standard pipeline from randomisation test

---
+++ random_data_ndrich.R
@@ -0,0 +1,74 @@
+source("nDrich.R")
+
+#Read in tsv file
+x<-as.matrix(read.table("rna_LGHGvHGHGV_jn.rnk",header=T))
+
+#read in GMT file
+#genesets<-GMT2DF("ReactomePathways.gmt")
+genesets<-gmt_import("ReactomePathways.gmt")
+
+#run the analysis
+res<-endrich(x,genesets,resrows=25)
+plot2DSets(res,outfile="outres.pdf")
+render_report(res,"outres.html")
+
+# run an example shiny document
+rmarkdown::run("test_shiny.Rmd")
+
+
+#randomise rownames test
+xx<-x
+rownames(xx)<-sample(rownames(x))
+head(xx)
+res<-endrich(xx,genesets)
+render_report(res,"randres_names.html")
+
+#randomise all data
+xx<-x
+xx[,1]<-sample(x[,1])
+xx[,2]<-sample(x[,2])
+head(xx)
+res<-endrich(xx,genesets)
+render_report(res,"randres_xy.html")
+
+
+#run some permutes
+PERMUTES=100
+numsig1=NULL
+print(paste("Running",PERMUTES,"permutes"))
+for (i in 1:PERMUTES) {
+set.seed(i+100)
+xx<-x
+rownames(xx)<-sample(rownames(x))
+head(xx)
+res<-endrich(xx,genesets)
+n<-length(which(res$manova_result$p.adjustMANOVA<0.05))
+numsig1=c(numsig1,n)
+}
+numsig1<-as.data.frame(numsig1)
+numsig1
+
+numsig2=NULL
+print(paste("Running",PERMUTES,"permutes"))
+for (i in 1:PERMUTES) {
+xx<-x
+set.seed(i+200)
+xx[,1]<-sample(x[,1])
+set.seed(i+300)
+xx[,2]<-sample(x[,2])
+res<-endrich(xx,genesets)
+n<-length(which(res$manova_result$p.adjustMANOVA<0.05))
+numsig2=c(numsig2,n)
+}
+numsig2<-as.data.frame(numsig2)
+numsig2
+
+MAX=max(c(max(numsig1),max(numsig2)))
+
+pdf("randres.pdf")
+par(mfrow=c(2,1))
+plot(numsig1$numsig1,main="Gene name randomisation",ylab="No. FDR MANOVA<0.05 sets",xlab="Run",pch=19,ylim=c(0,MAX))
+plot(numsig2$numsig2,main="Profile randomisation",ylab="No. FDR MANOVA<0.05 sets",xlab="Run",pch=19,ylim=c(0,MAX))
+dev.off()
+
+
