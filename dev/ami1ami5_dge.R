a9aa780b57aea83db2c9121d599cf9836bc0b32d
markziemann <mark.ziemann@gmail.com>
2019-02-06 23:19:56 +1100

    attaching gene names; work in progress

--- dev/ami1ami5_dge.R
+++ dev/ami1ami5_dge.R
@@ -1,6 +1,7 @@
 library("getDEE2")
 library("edgeR")
 library("DESeq2")
+source("nDrich.R")
 
 mdat<-getDee2Metadata("hsapiens")
 samples<-mdat[which(mdat$SRP_accession=="SRP096178"),]
@@ -12,45 +13,55 @@ SRRlist<-as.vector(samples$SRR_accession)
 x<-getDEE2("hsapiens",SRRlist)
 x<-Tx2Gene(x)
 y<-x$Tx2Gene
+
+#here attach genenames
+genenames<-unique(x$TxInfo[,1:2])
+rownames(genenames)=genenames$GeneID
+genenames$GeneID=NULL
+y<-merge(y,genenames,by=0)
+rownames(y)<-paste(y$Row.names,y$GeneSymbol,sep="|")
+y$Row.names=y$GeneSymbol=NULL
+
+#filter very low genes
 y<-y[which(rowSums(y)/ncol(y)>=(10)),]
 
 #EdgeR analysis
 design<-model.matrix(~samples$group)
 rownames(design)<-samples$SRR_accession
-y<-DGEList(counts=y)
-y<-calcNormFactors(y)
-y <- estimateDisp(y, design,robust=TRUE)
+z<-DGEList(counts=y)
+z<-calcNormFactors(y)
+z <- estimateDisp(y, design,robust=TRUE)
 
 #DE for AMI1
 dge=NULL
-fit <- glmFit(y, design[,1:2])
+fit <- glmFit(z, design[,1:2])
 lrt <- glmLRT(fit)
 dge<-as.data.frame(topTags(lrt,n=Inf))
 dge$dispersion<-lrt$dispersion
 dge<-merge(dge,lrt$fitted.values,by='row.names')
 rownames(dge)=dge$Row.names
 dge$Row.names=NULL
-dge<-merge(dge,y$counts,by='row.names')
+dge<-merge(dge,z$counts,by='row.names')
 ami1_edger<-dge[order(dge$PValue),]
-
+ami1_edger$GeneSymbol<-sapply((strsplit(ami1_edger$Row.names,"\\|")),"[[",2)
 
 #DE for AMI5
 dge=NULL
-fit <- glmFit(y, design[,c(1,3)])
+fit <- glmFit(z, design[,c(1,3)])
 lrt <- glmLRT(fit)
 dge<-as.data.frame(topTags(lrt,n=Inf))
 dge$dispersion<-lrt$dispersion
 dge<-merge(dge,lrt$fitted.values,by='row.names')
 rownames(dge)=dge$Row.names
 dge$Row.names=NULL
-dge<-merge(dge,y$counts,by='row.names')
+dge<-merge(dge,z$counts,by='row.names')
 ami5_edger<-dge[order(dge$PValue),]
+ami5_edger$GeneSymbol<-sapply((strsplit(ami5_edger$Row.names,"\\|")),"[[",2)
 
 save.image("ami1ami5_dge.RData")
 
 #now do DESeq2
-y<-x$Tx2Gene
-y<-round(y[which(rowSums(y)/ncol(y)>=(10)),])
+y<-round(y)
 
 #ami1
 des<-samples[1:6,]
@@ -64,6 +75,8 @@ zz<-cbind(z,assay(vsd))
 #sort by adjusted p-value
 zz<-as.data.frame(zz[order(zz$pvalue),])
 ami1_deseq2<-zz
+ami1_deseq2$GeneSymbol<-sapply((strsplit(rownames(ami1_deseq2),"\\|")),"[[",2)
+
 
 #AMI5
 des<-samples[c(1:3,7:9),]
@@ -77,6 +90,25 @@ zz<-cbind(z,assay(vsd))
 #sort by adjusted p-value
 zz<-as.data.frame(zz[order(zz$pvalue),])
 ami5_deseq2<-zz
+ami5_deseq2$GeneSymbol<-sapply((strsplit(rownames(ami5_deseq2),"\\|")),"[[",2)
 
 save.image("ami1ami5_dge.RData")
+
+#read in GMT file
+genesets<-gmt_import("ReactomePathways.gmt")
+
+#edger analysis
+x1<-list("ami1_edger"=ami1_edger,"ami5_edger"=ami5_edger)
+y1<-ndrich_import(x1,"edger",geneIDcol="GeneSymbol")
+res<-endrich(y1,genesets,resrows=25)
+plotSets(res,outfile="ami1ami5_edger.pdf")
+render_report(res,"ami1ami5_edger.html")
+
+#deseq analysis
+x2<-list("ami1_deseq2"=ami1_deseq2,"ami5_deseq2"=ami5_deseq2)
+y2<-ndrich_import(x2,"deseq2",geneIDcol="GeneSymbol")
+res<-endrich(y2,genesets,resrows=25)
+plotSets(res,outfile="ami1ami5_deseq.pdf")
+render_report(res,"ami1ami5_deseq.html")
+
 
